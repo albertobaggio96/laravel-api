fc1b688ae5b7586cb6bda71f0287d18d1b052438
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-02 14:43:53 +0100

    creation projects api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::all();
+
+        return response()->json([
+            'success'=> true,
+            'results'=> $projects
+        ]);
+    }
+}
